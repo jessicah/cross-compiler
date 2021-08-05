fe53c96a05da79a13d172fa32a50222f855c2adf
Jessica Hamilton <jessica.l.hamilton@gmail.com>
2021-08-05 00:02:14 +0000

    Allow building a hybrid toolchain.
    * Also bring patches in tree

--- build-rootfs.sh
+++ build-rootfs.sh
@@ -2,21 +2,73 @@
 
 set -e
 
+usage()
+{
+	echo "Usage: $0 BuildArch [--jobs <N>] [--rootfsdir <directory>]"
+	echo "BuildArch can be: x86, x86_64, x86h, x86_gcc2h"
+	echo "--rootfsdir dir - optional, defaults to current dir, where to"
+	echo "                  put cross-compiler and Haiku sysroot."
+	echo "--jobs N        - optional, restrict to N jobs."
+	exit 1
+}
+
 __InitialDir=$PWD
+__RootfsDir="$__InitialDir"
+
+case $1 in
+	x86|x86_64)
+		__BuildArch=$1
+		;;
+	x86h)
+		__BuildArch=x86
+		__BuildSecondaryArch=x86_gcc2
+		;;
+	x86_gcc2h)
+		__BuildArch=x86_gcc2
+		__BuildSecondaryArch=x86
+		;;
+	*)
+		usage
+		;;
+esac
+
+shift
+
+while :; do
+	if [ $# -le 0 ]; then
+		break
+	fi
+
+	case $1 in
+		--rootfsdir|-rootfsdir)
+			shift
+			__RootfsDir=$1
+			;;
+		--jobs|-jobs)
+			shift
+			MAXJOBS=$1
+			;;
+		*)
+			usage
+			;;
+	esac
+
+	shift
+done
+
 
 if [ -z "$__RootfsDir" ] && [ ! -z "$ROOTFS_DIR" ]; then
 	__RootfsDir=$ROOTFS_DIR
-else
-	__RootfsDir="$__InitialDir"
 fi
+
+echo "Using $__RootfsDir..."
 
 mkdir -p $__RootfsDir
 __RootfsDir="$( cd "$__RootfsDir" && pwd )"
 
 JOBS=${MAXJOBS:="$(getconf _NPROCESSORS_ONLN)"}
 
-# For now, assume we're building for x86_64
-echo 'Building Haiku sysroot for x86_64'
+echo "Building Haiku sysroot for $__BuildArch"
 mkdir -p "$__RootfsDir/tmp"
 pushd "$__RootfsDir/tmp"
 if [ ! -e "$__RootfsDir/tmp/haiku/.git" ]; then
@@ -35,11 +87,11 @@ fi
 cd "$__RootfsDir/tmp/haiku"
 git reset --hard origin/master
 ## use relative symlinks in _devel package
-git fetch origin refs/changes/18/4218/2 && git cherry-pick FETCH_HEAD
+git am "$__InitialDir/0001-haiku_devel-use-relative-symlinks-to-libraries-in-ha.patch"
 ## add development build profile (slimmer than nightly)
-git fetch origin refs/changes/64/4164/1 && git cherry-pick FETCH_HEAD
+git am "$__InitialDir/0002-Add-extra-build-profile-development.patch"
 ## add the patch for providing an explicit sysroot
-git am "$__InitialDir/0001-cross_tools-allow-specifying-a-custom-sysroot-path.patch"
+git am "$__InitialDir/0003-cross_tools-allow-specifying-a-custom-sysroot-path.patch"
 
 # Build jam
 echo 'Building jam buildtool'
@@ -50,7 +102,11 @@ make
 echo "Building cross tools with $JOBS parallel jobs"
 mkdir -p "$__RootfsDir/generated"
 cd "$__RootfsDir/generated"
-"$__RootfsDir/tmp/haiku/configure" -j"$JOBS" --sysroot "$__RootfsDir" --cross-tools-source "$__RootfsDir/tmp/buildtools" --build-cross-tools x86_64
+if [ -z "$__BuildSecondaryArch" ]; then
+	"$__RootfsDir/tmp/haiku/configure" -j"$JOBS" --sysroot "$__RootfsDir" --cross-tools-source "$__RootfsDir/tmp/buildtools" --build-cross-tools $__BuildArch
+else
+	"$__RootfsDir/tmp/haiku/configure" -j"$JOBS" --sysroot "$__RootfsDir" --cross-tools-source "$__RootfsDir/tmp/buildtools" --build-cross-tools $__BuildArch --build-cross-tools $__BuildSecondaryArch
+fi
 
 # Build haiku packages
 echo 'Building Haiku packages and package tool'
@@ -60,10 +116,12 @@ echo 'HAIKU_BUILD_PROFILE = "development-raw" ;' > UserProfileConfig
 # Setup the sysroot
 echo 'Extracting packages into sysroot'
 mkdir -p "$__RootfsDir/boot/system"
-for file in "$__RootfsDir/generated/objects/haiku/x86_64/packaging/packages/"*.hpkg; do
+for file in "$__RootfsDir/generated/objects/haiku/$__BuildArch/packaging/repositories/Haiku/packages/"*.hpkg; do
+	echo "Extracting $file..."
 	"$__RootfsDir/generated/objects/linux/x86_64/release/tools/package/package" extract -C "$__RootfsDir/boot/system" "$file"
 done
 for file in "$__RootfsDir/generated/download/"*.hpkg; do
+	echo "Extracting $file..."
 	"$__RootfsDir/generated/objects/linux/x86_64/release/tools/package/package" extract -C "$__RootfsDir/boot/system" "$file"
 done
 
