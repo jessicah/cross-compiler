eba4e4b6eea22daba40fc6be5cc16c0d3fcf6330
Jessica Hamilton <jessica.l.hamilton@gmail.com>
2026-05-08 21:55:46 +1200

    Update to be compatible with latest upstream.

--- build-rootfs.sh
+++ build-rootfs.sh
@@ -121,6 +121,7 @@ fi
 # Fetch some patches that haven't been merged yet
 cd "$__RootfsDir/tmp/haiku"
 git reset --hard review/master
+git am --abort || true
 git am "$__InitialDir/0003-build-add-an-optional-UserProfileConfig.patch"
 
 # Build jam
